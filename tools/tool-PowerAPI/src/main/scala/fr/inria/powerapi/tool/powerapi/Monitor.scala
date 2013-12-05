2d3c273aaf2a4f7f234830ab75acb7886a02fdd9
mascha <mascha.kurpicz@unine.ch>
2013-12-05 21:47:24 +0100

    adding new parameters for the powerapi tool: formula and sensor

--- tools/tool-PowerAPI/src/main/scala/fr/inria/powerapi/tool/powerapi/Monitor.scala
+++ tools/tool-PowerAPI/src/main/scala/fr/inria/powerapi/tool/powerapi/Monitor.scala
@@ -26,17 +26,31 @@ import fr.inria.powerapi.reporter.file.FileReporter
 import fr.inria.powerapi.reporter.jfreechart.JFreeChartReporter
 
 object Initializer {
-  def beforeStart() {
+  def beforeStart(sensor:String,formula:String) {
     Array(
-      classOf[fr.inria.powerapi.sensor.cpu.proc.CpuSensor],
-      classOf[fr.inria.powerapi.formula.cpu.max.CpuFormula]
+      sensor match {
+	case "cpu-proc" => classOf[fr.inria.powerapi.sensor.cpu.proc.CpuSensor]
+	case "cpu-proc-reg" => classOf[fr.inria.powerapi.sensor.cpu.proc.reg.CpuSensor] 	
+      }, 
+      formula match {
+	case "cpu-max" => classOf[fr.inria.powerapi.formula.cpu.max.CpuFormula]
+	case "cpu-max-vm" => classOf[fr.inria.powerapi.formula.cpu.maxvm.CpuFormula]
+	case "cpu-reg" => classOf[fr.inria.powerapi.formula.cpu.reg.CpuFormula]
+      }
     ).foreach(PowerAPI.startEnergyModule(_))
   }
 
-  def beforeEnd() {
+  def beforeEnd(sensor:String,formula:String) {
     Array(
-      classOf[fr.inria.powerapi.sensor.cpu.proc.CpuSensor],
-      classOf[fr.inria.powerapi.formula.cpu.max.CpuFormula]
+     sensor match {
+        case "cpu-proc" => classOf[fr.inria.powerapi.sensor.cpu.proc.CpuSensor]
+        case "cpu-proc-reg" => classOf[fr.inria.powerapi.sensor.cpu.proc.reg.CpuSensor]
+     },
+     formula match {
+        case "cpu-max" => classOf[fr.inria.powerapi.formula.cpu.max.CpuFormula]
+        case "cpu-maxvm" => classOf[fr.inria.powerapi.formula.cpu.maxvm.CpuFormula]
+        case "cpu-reg" => classOf[fr.inria.powerapi.formula.cpu.reg.CpuFormula]
+     }
     ).foreach(PowerAPI.stopEnergyModule(_))
   }
 }
@@ -47,7 +61,9 @@ object Monitor extends App {
   lazy val OutputFormat = """-output\s+(console|file|chart|virtio)""".r
   lazy val FileFormat   = """-filename\s+(\w+)""".r
   lazy val FreqFormat   = """-frequency\s+(\d+)""".r
-  
+  lazy val SensorFormat = """-sensor\s+(cpu-proc|cpu-proc-reg)""".r
+  lazy val FormulaFormat = """-formula\s+(cpu-max|cpu-max-vm|cpu-reg)""".r
+ 
   val params =
   (for (arg <- args) yield {
     arg match {
@@ -56,11 +72,13 @@ object Monitor extends App {
       case OutputFormat(out)    => ("out" -> out)
       case FileFormat(filePath) => ("filePath" -> filePath)
       case FreqFormat(freq)     => ("freq" -> freq)
+      case SensorFormat(sensor) => ("sensor" -> sensor)
+      case FormulaFormat(formula) => ("formula" -> formula)
       case _ => ("none" -> "")
     }
   }).toMap
-  
-  Initializer.beforeStart
+ 
+  Initializer.beforeStart(params.getOrElse("sensor","cpu-proc":String),params.getOrElse("formula","cpu-max":String))
   Processes.filePath = params.getOrElse("filePath", "powerapi-out": String)
   Processes.start(
     pids = params.getOrElse("pids", "-1": String).split(',').map(_.toInt),
@@ -68,6 +86,6 @@ object Monitor extends App {
     out = params.getOrElse("out", "chart": String),
     freq = params.getOrElse("freq", "1000": String).toInt
   )
-  Initializer.beforeEnd
+  Initializer.beforeEnd(params.getOrElse("sensor","cpu-proc":String),params.getOrElse("formula","cpu-max":String))
   System.exit(0)
 }
