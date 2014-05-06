cecd7eb37466cf537724f09f4837a819ec8d43e4
mcolmant <maxime.colmant@gmail.com>
2014-05-06 23:29:51 +0200

    experiments processing done

--- examples/exp-middleware/src/main/scala/fr/inria/powerapi/exp/middleware/Monitor.scala
+++ examples/exp-middleware/src/main/scala/fr/inria/powerapi/exp/middleware/Monitor.scala
@@ -110,6 +110,15 @@ class ExtendedFileReporter extends Reporter {
   }
 }
 
+object Tool {
+  // Method used to compute the median of any array type.
+  def median[T](s: Seq[T])(implicit n: Fractional[T]) = {
+    import n._
+    val (lower, upper) = s.sortWith(_<_).splitAt(s.size / 2)
+    if (s.size % 2 == 0) (lower.last + upper.head) / fromInt(2) else upper.head
+  }
+}
+
 /**
  * Object for simple experimentation, to observe the results directly in a chart.
  */
@@ -135,16 +144,16 @@ object Default {
  * Object for the experiments with SPEC CPU 2006.
  */
 object SpecCPUExp {
-  def run() = {
-    implicit val codec = scalax.io.Codec.UTF8
-    val benchmarks = Array("calculix", "soplex", "bzip2")
-    val path = "/home/colmant/cpu2006"
-    val duration = "30"
-    val dataPath = "host-spec-cpu-data"
-    val warmup = 5
-    val nbRuns = warmup + 5
-    val separator = "====="
+  implicit val codec = scalax.io.Codec.UTF8
+  val benchmarks = Array("calculix", "soplex", "bzip2")
+  val path = "/home/colmant/cpu2006"
+  val duration = "30"
+  val dataPath = "host-spec-cpu-data"
+  val warmup = 0
+  val nbRuns = warmup + 3
+  val separator = "====="
 
+  def collect() = {
     // Cleaning phase
     Path.fromString(dataPath).deleteRecursively(force = true)
     (Path(".") * "*.dat").foreach(path => path.delete(force = true))
@@ -174,7 +183,7 @@ object SpecCPUExp {
         (Path(".") * "*.dat").foreach(path => path.append(separator + scalax.io.Line.Terminators.NewLine.sep))
 
         // Launch the benchmark with a bash script (easiest way, and blocking).
-        Seq("bash", "./src/main/resources/start_bench.bash", path, benchmark, duration).!
+        Seq("bash", "./src/main/resources/start_bench.bash", path, benchmark).!
 
         // For the moment, is the only way to stop powerapi.
         monitoringLibpfm.waitFor(1.milliseconds)
@@ -195,20 +204,145 @@ object SpecCPUExp {
 
     LibpfmUtil.terminate()
   }
+
+  def process() = {
+    implicit val codec = scalax.io.Codec.UTF8
+
+    lazy val PathRegex = (s"$dataPath" + """/\w+/(\d+)/.*""").r
+    // Method used to sort the paths.
+    def sortPaths(path1: Path, path2: Path) = {
+      val nb1 = path1.path match {
+        case PathRegex(benchmark) => benchmark
+        case _ => ""
+      }
+      val nb2 = path2.path match {
+        case PathRegex(benchmark) => benchmark
+        case _ => ""
+      }
+
+      nb1.compareTo(nb2) < 0
+    }
+
+    val estimationPaths = (Path.fromString(dataPath) * """\w+""".r * """\d+""".r * "powerapi_cpu.dat")
+    val powerPaths = (Path.fromString(dataPath) * """\w+""".r * """\d+""".r * "powerapi_powerspy.dat")
+
+    // Get the data.
+    val data = scala.collection.mutable.HashMap[Path, Array[String]]()
+    estimationPaths.foreach(path => {
+      data(path) = path.lines().toArray
+    })
+    val powers = scala.collection.mutable.HashMap[Path, Array[String]]()
+    powerPaths.foreach(path => {
+      powers(path) = path.lines().toArray  
+    })
+
+    val csvData = scala.collection.mutable.LinkedHashMap[Int, scala.collection.mutable.ArrayBuffer[String]]()
+
+    // Create the headers.
+    csvData(0) = scala.collection.mutable.ArrayBuffer[String]()
+    csvData(1) = scala.collection.mutable.ArrayBuffer[String]()
+
+    benchmarks.sorted.foreach(elt => {
+      csvData(0) += "# " + elt
+      csvData(1) ++= Array("#Estimated", "# Measured", "#min", "#max")
+    })
+
+    val nbLinesDataCSV = csvData.keys.size
+
+    for(benchmark <- benchmarks.sorted) {
+      // Organize the data.
+      val estimatedData = scala.collection.mutable.HashMap[Int, scala.collection.mutable.ArrayBuffer[Double]]()
+      estimationPaths.toArray.filter(_.path.contains(benchmark)).sortWith((path1, path2) => sortPaths(path1, path2)).foreach(path => {
+        var index = 0
+        // Remove the synchronization phase
+        data(path) = data(path).dropWhile(_ != separator).tail
+        
+        while(!data(path).isEmpty) {
+          val existing = estimatedData.getOrElse(index, scala.collection.mutable.ArrayBuffer[Double]())
+          val stepPowers = data(path).takeWhile(_ != separator).filter(line => (line != "" && line != "0")).map(_.split("=").last.toDouble).filter(value => value > 0 && value < 1000)
+          existing += stepPowers.sum
+          estimatedData(index) = existing
+          // tail is used to remove the separator.
+          val tmpData = data(path).dropWhile(_ != separator)
+
+          if(!tmpData.isEmpty) {
+            data(path) = tmpData.tail
+          }
+
+          else data(path) = tmpData
+
+          index += 1
+        }
+      })
+      val powerspyData = scala.collection.mutable.HashMap[Int, scala.collection.mutable.ArrayBuffer[Double]]()
+      powerPaths.toArray.filter(_.path.contains(benchmark)).sortWith((path1, path2) => sortPaths(path1, path2)).foreach(path => {
+        var index = 0
+        // Remove the synchronization phase
+        powers(path) = powers(path).dropWhile(_ != separator).tail
+
+        while(!powers(path).isEmpty) {
+          val existing = powerspyData.getOrElse(index, scala.collection.mutable.ArrayBuffer[Double]())
+          val stepPowers = powers(path).takeWhile(_ != separator).filter(line => (line != "" && line != "0")).map(_.split("=").last.toDouble).filter(value => value > 0 && value < 1000)
+          existing += stepPowers.sum
+          powerspyData(index) = existing
+          // tail is used to remove the separator.
+          val tmpData = powers(path).dropWhile(_ != separator)
+
+          if(!tmpData.isEmpty) {
+            powers(path) = tmpData.tail
+          }
+
+          else powers(path) = tmpData
+
+          index += 1
+        }
+      })
+
+      //println(estimatedData)
+
+      for(i <- 0 until estimatedData.keys.size) {
+        val consumption = Tool.median(estimatedData(i))
+        val existing = csvData.getOrElse(nbLinesDataCSV + i, scala.collection.mutable.ArrayBuffer[String]())
+        existing += consumption.toString
+        csvData(nbLinesDataCSV + i) = existing
+      }
+
+      for(i <- 0 until powerspyData.keys.size) {
+        val min = powerspyData(i).min
+        val max = powerspyData(i).max
+        val consumption = Tool.median(powerspyData(i))
+        val existing = csvData.getOrElse(nbLinesDataCSV + i, scala.collection.mutable.ArrayBuffer[String]())
+        existing += consumption.toString
+        existing += min.toString
+        existing += max.toString
+        csvData(nbLinesDataCSV + i) = existing
+      }
+    }
+
+    // Cleaning phase
+    Path.fromString(s"$dataPath/chart.dat").delete()
+    csvData.values.foreach(line => {
+      Resource.fromFile(s"$dataPath/chart.dat").append(line.mkString(" ") + scalax.io.Line.Terminators.NewLine.sep)
+    })
+  }
+
+  def run() = {
+    collect()
+    process()
+  }
 }
 
 /**
  * Object used for the experiments with stress command, to show the non-linearity of complex processors.
  */
 object StressExp extends StressExpConfiguration {
-
+  val dataPath = "host-stress-data"
+  val separator = "====="
+  val duration = "30"
+  val warmup = 0
+  val nbRuns = warmup + 3
+  
   def collect() = {
-    val duration = "30"
-    val dataPath = "host-stress-data"
-    val warmup = 0
-    val nbRuns = warmup + 3
-    val separator = "====="
-
     Path.fromString(dataPath).deleteRecursively(force = true)
     (Path(".") * "*.dat").foreach(path => path.delete(force = true))
 
@@ -222,8 +356,8 @@ object StressExp extends StressExpConfiguration {
       var monitoringLibpfm = libpfm.start(ALL(), 1.seconds).attachReporter(classOf[ExtendedFileReporter])
       val monitoringPspy = powerspy.start(PIDS(-1), 1.seconds).attachReporter(classOf[ExtendedFileReporter])
 
-      // Waiting for the synchronization.
-      Thread.sleep((20.seconds).toMillis)
+      // Waiting for the synchronization and to get idle powers.
+      Thread.sleep((40.seconds).toMillis)
       (Path(".") * "*.dat").foreach(path => path.append(separator + scalax.io.Line.Terminators.NewLine.sep))
 
       for(thread <- 1 to threads) {
@@ -251,19 +385,87 @@ object StressExp extends StressExpConfiguration {
   }
 
   def process() = {
+    implicit val codec = scalax.io.Codec.UTF8
 
+    lazy val PathRegex = (s"$dataPath" + """/(\d)+/.*""").r
+    // Method used to sort the paths.
+    def sortPaths(path1: Path, path2: Path) = {
+      val nb1 = path1.path match {
+        case PathRegex(nb) => nb.toDouble
+        case _ => 0.0
+      }
+      val nb2 = path2.path match {
+        case PathRegex(nb) => nb.toDouble
+        case _ => 0.0
+      }
+
+      nb1.compareTo(nb2) < 0
+    }
+
+    val estimationPaths = (Path.fromString(dataPath) * """\d+""".r * "powerapi_cpu.dat")
+
+    // Get the data.
+    val data = scala.collection.mutable.HashMap[Path, Array[String]]()
+    estimationPaths.foreach(path => {
+      data(path) = path.lines().toArray
+    })
+
+    val csvData = scala.collection.mutable.LinkedHashMap[Int, scala.collection.mutable.ArrayBuffer[String]]()
+
+    // Create the headers.
+    csvData(0) = scala.collection.mutable.ArrayBuffer[String]()
+    Array("# CPU Load", "# Estimated consumption").foreach(elt => csvData(0) += elt)
+
+    val nbLinesDataCSV = csvData.keys.size
+
+    // Organize the data.
+    val estimatedData = scala.collection.mutable.HashMap[Int, scala.collection.mutable.ArrayBuffer[Double]]()
+    estimationPaths.toArray.sortWith((path1, path2) => sortPaths(path1, path2)).foreach(path => {
+      var index = 0
+      while(!data(path).isEmpty) {
+        val existing = estimatedData.getOrElse(index, scala.collection.mutable.ArrayBuffer[Double]())
+        existing ++= data(path).takeWhile(_ != separator).filter(line => (line != "" && line != "0")).map(_.split("=").last.toDouble).filter(value => value > 0 && value < 1000)
+        estimatedData(index) = existing
+        // tail is used to remove the separator.
+        val tmpData = data(path).dropWhile(_ != separator)
+
+        if(!tmpData.isEmpty) {
+          data(path) = tmpData.tail
+        }
+
+        else data(path) = tmpData
+
+        index += 1
+      }
+    })
+
+    // Compute the medians and store values inside the corresponding csv buffer.
+    for(i <- 0 until estimatedData.keys.size) {
+      // +1 because of the header
+      val medianVal = Tool.median(estimatedData(i))
+      val existing = csvData.getOrElse(nbLinesDataCSV + i, scala.collection.mutable.ArrayBuffer[String]())
+      existing += (i.toDouble / (estimatedData.keys.size - 1)).toString
+      existing += medianVal.toString
+      csvData(nbLinesDataCSV + i) = existing
+    }
+
+    // Cleaning phase
+    Path.fromString(s"$dataPath/chart.dat").delete()
+    csvData.values.foreach(line => {
+      Resource.fromFile(s"$dataPath/chart.dat").append(line.mkString(" ") + scalax.io.Line.Terminators.NewLine.sep)
+    })
   }
 
   def run() = {
     collect()
-    //process()
+    process()
   }
 }
 
 // Object launcher.
 object Monitor extends App {
   //Default.run()
-  //SpecCPUExp.run()
+  SpecCPUExp.run()
   StressExp.run()
   System.exit(0)
 }
