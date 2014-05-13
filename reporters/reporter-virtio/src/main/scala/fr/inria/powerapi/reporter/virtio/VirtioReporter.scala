4dcf8fd07529a1dfb47bfc5aee871d0c8dbd98c6
powerapi <maxime.colmant@gmail.com>
2014-05-13 16:30:41 +0200

    fix VirtioReporter (virtio serial), problem with the synchronization with libfpm

--- reporters/reporter-virtio/src/main/scala/fr/inria/powerapi/reporter/virtio/VirtioReporter.scala
+++ reporters/reporter-virtio/src/main/scala/fr/inria/powerapi/reporter/virtio/VirtioReporter.scala
@@ -35,6 +35,8 @@ import org.newsclub.net.unix.AFUNIXSocketException
 import scala.collection.JavaConversions
 import com.typesafe.config.Config
 
+import org.apache.log4j.Level
+
 /**
  * Listen to AggregatedMessage and send its content to the virtual machine using VirtioSerial.
  */
@@ -47,14 +49,6 @@ trait Configuration extends fr.inria.powerapi.core.Configuration {
 
 class VirtioReporter(configuration: Map[Int, Int] = Map[Int, Int]()) extends Reporter with Configuration {
   lazy val sockets = scala.collection.mutable.Map.empty[Int, AFUNIXSocket]
-  
-  for((vmPid, port) <- configuration) {
-    if (log.isInfoEnabled) log.info("vmPID: " + vmPid + "and port: " + port)
-    var sock = AFUNIXSocket.newInstance()
-    var socketaddress = new AFUNIXSocketAddress(new File(socketpath + "port" + port))
-    sock.connect(socketaddress)
-    sockets(vmPid) = sock
-  }
 
   case class Line(processedMessage: ProcessedMessage) {
     override def toString() =
@@ -65,9 +59,40 @@ class VirtioReporter(configuration: Map[Int, Int] = Map[Int, Int]()) extends Rep
   }
 
   def process(processedMessage: ProcessedMessage) {
-    val os = sockets(processedMessage.tick.subscription.process.pid).getOutputStream()
-    val data = processedMessage.energy.power.toString+"\n"
-    os.write(data.getBytes())
-    if (log.isInfoEnabled) log.info(Line(processedMessage).toString)
+    val vmPid = processedMessage.tick.subscription.process.pid
+
+    // init.
+    if(!sockets.contains(vmPid)) {
+      if(configuration.contains(vmPid)) {
+        val port = configuration(vmPid)
+        if (log.isInfoEnabled) log.info("vmPID: " + vmPid + "and port: " + port)
+        var sock = AFUNIXSocket.newInstance()
+        try {
+          var socketaddress = new AFUNIXSocketAddress(new File(socketpath + "port" + port))
+          sock.connect(socketaddress)
+          sockets(vmPid) = sock
+        }
+        catch {
+          case _: Exception => if (logger.isEnabledFor(Level.WARN)) logger.warn("Connexion impossible with the VM: " + vmPid)
+        }
+      }
+    }
+
+    // If it's correctly set, write the data in the buffer.
+    if(sockets.contains(vmPid)) {
+      val data = processedMessage.energy.power.toString + "\n"
+      
+      try {
+        val os = sockets(vmPid).getOutputStream()
+        os.write(data.getBytes())
+      }
+      catch {
+        case _: Exception =>
+          if (logger.isEnabledFor(Level.WARN)) logger.warn("Connexion lost with the VM: " + vmPid)
+          sockets -= vmPid
+      }
+
+      if (log.isInfoEnabled) log.info(Line(processedMessage).toString)
+    }
   }
 }
